1094df8ec93172b259a852cbcaab370a278b1390
Samir Hasan <sam1487@gmail.com>
2015-11-24 02:38:50 -0600

    added database

--- home.php
+++ home.php
@@ -1,62 +1,66 @@
 <?php
 include 'database.php';
 $con = getConnection();
+$tables = new array('Book', 'Customer', 'OrderDetail', 'Orders', 'Shipper', 'Subject', 'Supplier');
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Practical CSS3 tables with rounded corners - demo</title>
+    <title>COMP-6120: Term Project</title>
     <link rel='stylesheet' href='style.css' type='text/css' media='all' />
 </head>
 
 <body>
 
-<h2>Book</h2>
-<table class="bordered">
-    <thead>
-    <?php
-    $query = 'SELECT * FROM Book';
-    $result = executeQuery($con, $query);  
-    if(!$result) {
-        die('Query failed to execute: ' . mysqli_error($con));   
-    }
-    //$books = mysqli_fetch_assoc($result);
-    $numFields = mysqli_num_fields($result);
+<?php 
+foreach($tables as $tableName) {
+    <h2>$tableName</h2>
+    <table class="bordered">
+        <thead>
+        <?php
+        $query = 'SELECT * FROM '. $tableName;
+        $result = executeQuery($con, $query);  
+        if(!$result) {
+            die('Query failed to execute: ' . mysqli_error($con));   
+        }
+        //$books = mysqli_fetch_assoc($result);
+        $numFields = mysqli_num_fields($result);
 
-    echo '<tr>';
-    for($i = 0; $i < $numFields; $i++) {
-        $field = mysqli_fetch_field_direct($result, $i);
-        echo '<th>' . $field->name . '</th>';
-    }
-    echo '</tr>';
-
-
-    ?>
-    </thead>
-    
-    <?php
-    $books = array();
-    while($row = mysqli_fetch_assoc($result)) {
-        $books[] = $row;
-    }
-    foreach($books as $row) {
         echo '<tr>';
-        foreach($row as $col) {
-            echo '<td>' . $col . '</td>';        
+        for($i = 0; $i < $numFields; $i++) {
+            $field = mysqli_fetch_field_direct($result, $i);
+            echo '<th>' . $field->name . '</th>';
         }
         echo '</tr>';
-    }
-    
-    mysqli_free_result($result);
-
-    ?>
-
-</table>
-
-<br><br>
 
 
+        ?>
+        </thead>
+        
+        <?php
+        $rows = array();
+        while($resultRow = mysqli_fetch_assoc($result)) {
+            $rows[] = $resultRow;
+        }
+        foreach($rows as $row) {
+            echo '<tr>';
+            foreach($row as $col) {
+                echo '<td>' . $col . '</td>';        
+            }
+            echo '</tr>';
+        }
+        
+        mysqli_free_result($result);
+
+        ?>
+
+    </table>
+
+    <br><br>
+<?php
+}
+?>
 </body>
 </html>
 <?php mysqli_close($con); ?>
