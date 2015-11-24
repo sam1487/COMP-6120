f7046ad5a3e6b252cef88cd2da6911bee3c49540
Samir Hasan <sam1487@gmail.com>
2015-11-24 02:56:16 -0600

    added database

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ $tables = array('Book', 'Customer', 'OrderDetail', 'Orders', 'Shipper', 'Subject
 <body>
     <div style="text-align: center; height: 50px;; border-bottom: 1px solid grey">
         <a href='index.php' style = "border-radius: 5px; background: lightblue; width: 100px; height: 25px; padding: 10px;">All Tables</a>
-        <a href='' style = "border-radius: 5px; background: lightblue; width: 100px; height: 25px; padding: 10px;">Query Database</a>
+        <a href='query.php' style = "border-radius: 5px; background: lightblue; width: 100px; height: 25px; padding: 10px;">Query Database</a>
     </div>
 
 
