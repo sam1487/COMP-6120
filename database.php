5fc3e195d1cf34b6fa2157f5b87eed59db9d42b4
Samir Hasan <sam1487@gmail.com>
2015-11-23 23:21:39 -0600

    added database

---
+++ database.php
@@ -0,0 +1,54 @@
+<?php
+
+/* connect to database */
+
+$dbhost = "localhost";
+$dbuser = "szh0064";
+$dbpass = "sam1487";
+$dbname = "szh0064db";
+
+function getConnection() {
+	global $dbhost, $dbuser, $dbpass, $dbname;
+	$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	if(!$con) {
+		die('Could not connect: ' . mysqli_error(con));
+	}
+	return $con;
+}
+
+funciton executeQuery($con, $query) {
+	$con = getConnection();
+	$result = mysqli_query($con, $query);
+	if(!$result) {
+		die('Query does not produce any result');
+	}
+
+	return $result;
+}
+
+function countAffectedRows($con) {
+	return mysqli_affected_rows($con);
+}
+
+function getError($con) {
+	return mysqli_error($con);
+}
+
+//	while ($row = mysqli_fetch_row($result))//
+	//echo $row[0]; 
+
+	//while ($row = mysqli_fetch_assoc($result))
+	//echo $row['column_name'];
+
+	//$num = mysqli_$affected_rows($con);
+
+	/* detect error */
+
+	//$str = mysqli_connect_error();
+	//$err = mysqli_connect_errno();
+
+	//$str = mysqli_error($con);
+	//$err = mysqli_errno($con);
+}
+
+?>
